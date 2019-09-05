660b5ed6ecb788b67b2768e742c6e13f9e9c8fc0
LAO <liaozefeng@hotmail.com>
2019-09-05 23:42:25 +0800

    LAO

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MarketRateSpider
 A spider for market rate incl. swap, Libor, USDCNY etc.
+
+License
+This work is licensed under a MIT license.
---
+++ Swap.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://seb.se/pow/apps/swaprates/default.aspx'
+response = requests.get(url)
+soup = BeautifulSoup(response.content,'lxml')
+table = soup.find_all('table')[9]
+th = table.find_all('th')
+tr = table.tbody
+head = []
+for each in th:
+    head.append(each.text.strip())
+print(head)
+
+maturity = []
+swap = []
+date = []
+
+for each in tr:
+    if each == '\n':
+        next
+    else:
+        maturity.append(each.find('td',class_="first").text.strip())
+        swap.append(each.find('td',class_="value-cell").text.strip())
+        date.append(each.find('td',class_="value-cell last").text.strip())
+print(maturity,swap,date)
+
+dic1={head[0]:maturity, head[1]:swap, head[4]:date}
+df = pd.DataFrame(dic1)
+print(df)
+#By LAO
